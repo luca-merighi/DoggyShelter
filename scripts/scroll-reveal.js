d6b3bcabf6b570d8c425eba899817800cc486c3f
Luca Merighi <73050019+luca-merighi@users.noreply.github.com>
2022-05-11 22:28:58 -0300

    Add files via upload

---
+++ scripts/scroll-reveal.js
@@ -0,0 +1,15 @@
+ScrollReveal({
+    origin: 'top',
+    distance: '30px',
+    duration: 700,
+    reset: true
+}).reveal(`
+    .header,
+    #home,
+    #about,
+    #services,
+    #testimonials,
+    #location,
+    #contacts,
+    .footer
+`)
